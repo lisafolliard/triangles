8318da8c7dead0d16d7f7b95929775c6c83e84d8
tyler finley & lisa folliard <student@epicodus.com>
2015-08-10 10:53:24 -0700

    Create non_triangle? method and pass spec

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -13,7 +13,12 @@ class Triangles
   define_method(:isosceles?) do
     @side1.eql?(@side2) || @side1.eql?(@side3) || @side2.eql?(@side3)
   end
+
   define_method(:scalene?) do
    @side1 != @side2 && @side1 != @side3 && @side2 != @side3
- end
- end
+  end
+
+  define_method(:non_triangle?) do
+    @side1.+(@side2) <= (@side3) || @side2.+(@side3) <= (@side1) || @side3.+(@side1) <= (@side2)
+  end
+end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -12,8 +12,14 @@ describe('String#triangles') do
     isosceles_triangle = Triangles.new(4,4,5)
     expect(isosceles_triangle.isosceles?()).to(eq(true))
   end
+
   it('determines if no sides of triangle are equal') do
     scalene_triangle = Triangles.new(3,4,5)
     expect(scalene_triangle.scalene?()).to(eq(true))
+  end
+
+  it('determines if the object is not a triangle') do
+    non_triangle = Triangles.new(3,5,10)
+    expect(non_triangle.non_triangle?()).to(eq(true))
   end
 end

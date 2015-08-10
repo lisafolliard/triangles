84fb7cd3f157883e5d74806f83c030025e959ed5
tyler finley & lisa folliard <student@epicodus.com>
2015-08-10 10:19:56 -0700

    pass spec and define method for scalene triangle.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -13,4 +13,7 @@ class Triangles
   define_method(:isosceles?) do
     @side1.eql?(@side2) || @side1.eql?(@side3) || @side2.eql?(@side3)
   end
-end
+  define_method(:scalene?) do
+   @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+ end
+ end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -12,4 +12,8 @@ describe('String#triangles') do
     isosceles_triangle = Triangles.new(4,4,5)
     expect(isosceles_triangle.isosceles?()).to(eq(true))
   end
+  it('determines if no sides of triangle are equal') do
+    scalene_triangle = Triangles.new(3,4,5)
+    expect(scalene_triangle.scalene?()).to(eq(true))
+  end
 end
